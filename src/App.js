889d3945811d21e3d392875db58da5f7442551b9
priyankaagrawal1090 <priyankaagrawal1090@gmail.com>
2023-11-13 08:43:47 -0800

    Commented out home component temporarily and render static PatientUI.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import { Link } from 'react-scroll';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import PatientUI from './components/PatientUI'
 
 export const HOME_CONTENT_ITEMS = [
   {
@@ -27,41 +28,51 @@ export const HOME_CONTENT_ITEMS = [
   },
 ];
 
+// FIXME: Temporarily comment out Home/ and render static PatientUI
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <Navbar bg="dark" variant="dark" className="fixed-top">
+//           <Container>
+//             <Navbar.Brand href="#">Health Journal</Navbar.Brand>
+//             <Nav className="ml-auto">
+//               {/* Use Link for smooth scrolling */}
+//               {HOME_CONTENT_ITEMS.map((menu) => (
+//                 <Link 
+//                   to={menu.title}
+//                   smooth={true}
+//                   offset={-200}
+//                   duration={500}
+//                   delay={0}
+//                 >
+//                   <Nav.Item as="div" className="nav-item">{menu.title}</Nav.Item>
+//                 </Link>
+//               ))}
+//             </Nav>
+//           </Container>
+//         </Navbar>
+//       </header>
+      
+//       <div style={{ paddingTop: '50px' }}>
+//         {HOME_CONTENT_ITEMS.map((menu) => (
+//           <div className="content overlay" id={menu.title}>
+//             {<menu.component/>}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+
+//   );
+// }
+
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Navbar bg="dark" variant="dark" className="fixed-top">
-          <Container>
-            <Navbar.Brand href="#">Health Journal</Navbar.Brand>
-            <Nav className="ml-auto">
-              {/* Use Link for smooth scrolling */}
-              {HOME_CONTENT_ITEMS.map((menu) => (
-                <Link 
-                  to={menu.title}
-                  smooth={true}
-                  offset={-200}
-                  duration={500}
-                  delay={0}
-                >
-                  <Nav.Item as="div" className="nav-item">{menu.title}</Nav.Item>
-                </Link>
-              ))}
-            </Nav>
-          </Container>
-        </Navbar>
-      </header>
-      
-      <div style={{ paddingTop: '50px' }}>
-        {HOME_CONTENT_ITEMS.map((menu) => (
-          <div className="content overlay" id={menu.title}>
-            {<menu.component/>}
-          </div>
-        ))}
-      </div>
+    <div className='App'>
+      <PatientUI/>
     </div>
-
-  );
+  )
 }
 
 export default App;
