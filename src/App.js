9306272c2fa7e02d29429d8aa7c7ffe3eb770cec
priyankaagrawal1090 <priyankaagrawal1090@gmail.com>
2023-11-26 21:18:41 -0800

    Created Home component

--- src/App.js
+++ src/App.js
@@ -1,83 +1,21 @@
+import React from "react";
 import Home from "./components/Home";
-import About from "./components/About";
-import SignIn from "./components/SignIn";
-import AuthForm from "./components/AuthForm";
 import "font-awesome/css/font-awesome.min.css";
-import { Link as ScrollLink } from "react-scroll";
-import { Navbar, Nav, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import PatientUI from "./components/PatientUI";
-import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-export const HOME_CONTENT_ITEMS = [
-  {
-    id: 1,
-    title: "Home",
-    description: "Welcome to our application",
-    component: Home,
-  },
-  {
-    id: 2,
-    title: "About",
-    description: "Learn more about our application",
-    component: About,
-  },
-  {
-    id: 3,
-    title: "Sign In",
-    description: "Explore more by signing in",
-    component: AuthForm,
-  },
-];
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <Navbar bg="dark" variant="dark" className="fixed-top">
-            <Container>
-              <Navbar.Brand href="#">Health Journal</Navbar.Brand>
-              <Nav className="ml-auto">
-                {/* Use Link for smooth scrolling */}
-                {HOME_CONTENT_ITEMS.map((menu) => (
-                  <ScrollLink
-                    to={menu.title}
-                    smooth={true}
-                    offset={-200}
-                    duration={500}
-                    delay={0}
-                  >
-                    <Nav.Item as="div" className="nav-item">
-                      {menu.title}
-                    </Nav.Item>
-                  </ScrollLink>
-                ))}
-              </Nav>
-            </Container>
-          </Navbar>
-        </header>
-        <div style={{ paddingTop: "50px" }}>
-          <div className="content overlay">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/patient-ui" element={<PatientUI />} />
-              <Route path="/" element={<Login />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/patient-ui" element={<PatientUI />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
-// function App() {
-//   return (
-//     <>
-//       <PatientUI />
-//     </>
-//   );
-// }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,61 @@
-import React from 'react';
-import { HorizontalNavbar } from './Navbar';
-import PatientUI from './PatientUI';
+import React from "react";
+import { HorizontalNavbar } from "./Navbar";
+import PatientUI from "./PatientUI";
+import Login from "./Login";
+import About from "./About";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { Link as ScrollLink } from "react-scroll";
+
+export const HOME_CONTENT_ITEMS = [
+  {
+    id: 0,
+    title: "Sign In",
+    description: "Explore more by signing in",
+    component: Login,
+  },
+  {
+    id: 1,
+    title: "About",
+    description: "Learn more about our application",
+    component: About,
+  },
+];
 
 export default function Home() {
-    return (
-        <h1>Home from component</h1>
-    )
+  return (
+    <div style={{ paddingTop: "50px" }}>
+      <Navbar bg="dark" variant="dark" className="fixed-top">
+        <Container>
+          <Navbar.Brand href="#">
+            <ScrollLink to="0" className="navbar-brand">
+              Health Journal
+            </ScrollLink>
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            {/* Use Link for smooth scrolling */}
+            {HOME_CONTENT_ITEMS.map((menu) => (
+              <ScrollLink
+                to={menu.id}
+                smooth={true}
+                offset={-200}
+                duration={500}
+                delay={0}
+              >
+                <Nav.Item as="div" className="nav-item">
+                  {menu.title}
+                </Nav.Item>
+              </ScrollLink>
+            ))}
+          </Nav>
+        </Container>
+      </Navbar>
+      <div id="0" className="content overlay">
+        <Login />
+      </div>
+      <div id="1" className="content overlay">
+        <About />
+      </div>
+    </div>
+  );
 }
