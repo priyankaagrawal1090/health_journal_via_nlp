7654228ec840ef2e0195a6b1547b2e8a647420bc
priyankaagrawal1090 <priyankaagrawal1090@gmail.com>
2024-02-18 20:33:25 -0800

    Add onKeyDown handler in chatbox

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,83 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { HfInference } from '@huggingface/inference';
-import '../App.css';
+import { HfInference } from "@huggingface/inference";
+import "../App.css";
 
 export default class Chatbox extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-            userInput: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      userInput: "",
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      userInput: e.target.value,
+    });
+  };
+
+  handleSendMessage = async () => {
+    const hf = new HfInference("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");
+    const { userInput, messages } = this.state;
+    // const { messages, userInput } = this.state;
+
+    if (userInput.trim() === "") return; // Prevent sending empty messages
+
+    // Add the user's message to the list
+    let updateMessages = [...messages, { text: userInput, user: "user" }];
+
+    this.setState({ messages: updateMessages, userInput: "" });
+
+    let output = await hf.textGeneration({
+      model: "mistralai/Mistral-7B-Instruct-v0.2",
+      inputs: userInput,
+    });
+
+    //Add the response to the list
+    updateMessages = [
+      ...this.state.messages,
+      { text: output["generated_text"], user: "bot" },
+    ];
+
+    this.setState({ messages: updateMessages });
+
+    // Simulate a response (for demo only)
+    // setTimeout(() => {
+    //     let response = 'Sample response from bot';
+
+    //     // Add the response to the list
+    //     const updateMessages = [...this.state.messages, { text: response, user: 'bot'}];
+
+    //     this.setState({ messages: updateMessages });
+    // }, 1000);
+  };
+
+  handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevent default form submission behavior
+      this.handleSendMessage();
     }
+  };
 
-    handleInputChange = (e) => {
-        this.setState({
-            userInput: e.target.value
-        })
-    }
+  renderMessages() {
+    return this.state.messages.map((message, index) => (
+      <div key={index} className={`${message.user}`}>
+        <div className={`${message.user}-bubble`}>{message.user}</div>
+        {/* <div className={`${message.user}-text-bubble`}>{message.user}</div> */}
+        <div className="message-text">{message.text}</div>
+      </div>
+    ));
+  }
 
-    handleSendMessage = async () => {
-        const hf = new HfInference('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');
-        const { userInput, messages } = this.state;
-        // const { messages, userInput } = this.state;
-
-        if (userInput.trim() === '') return; // Prevent sending empty messages
-
-        // Add the user's message to the list
-        let updateMessages = [...messages, { text: userInput, user: 'user'}];
-
-        this.setState({ messages: updateMessages, userInput: '' });
-
-        let output = await hf.textGeneration({
-            model: 'mistralai/Mistral-7B-Instruct-v0.2',
-            inputs: userInput,
-        });
-
-        //Add the response to the list
-        updateMessages = [...this.state.messages, { text: output['generated_text'], user: 'bot'}];
-
-        this.setState({ messages: updateMessages });
-
-        // Simulate a response (for demo only)
-        // setTimeout(() => {
-        //     let response = 'Sample response from bot';
-
-        //     // Add the response to the list
-        //     const updateMessages = [...this.state.messages, { text: response, user: 'bot'}];
-
-        //     this.setState({ messages: updateMessages });
-        // }, 1000);
-    }
-
-    renderMessages() {
-        return this.state.messages.map((message, index) => (
-            <div key={index} className={`${message.user}`}>
-                <div className={`${message.user}-bubble`}>{message.user}</div>
-                {/* <div className={`${message.user}-text-bubble`}>{message.user}</div> */}
-                <div className="message-text">{message.text}</div>
-            </div>
-        ));
-    }
-
-    render() {
-        return (
-            <div className="chat-container">
-                <div className="chat-input">
-                    <input
-                        type="text"
-                        placeholder="Type a message..."
-                        value={this.state.userInput}
-                        onChange={this.handleInputChange}
-                    />
-                    <button onClick={this.handleSendMessage}>Send</button>
-                </div>
-                <div className="chat-messages">
-                    {this.renderMessages()}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="chat-container">
+        <div className="chat-input">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={this.state.userInput}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          <button onClick={this.handleSendMessage}>Send</button>
+        </div>
+        <div className="chat-messages">{this.renderMessages()}</div>
+      </div>
+    );
+  }
 }
