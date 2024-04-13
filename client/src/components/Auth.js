853eb51103279e96275e67439a740e6fe5aa1804
Ganesh Susarla <ganeshsusarla01@gmail.com>
2024-04-13 16:59:38 +0530

    Remove extra auth button from signin

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -9,13 +9,11 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "./dialog"
 import { useToast } from "./use-toast"
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "./select"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "./select"
 import { useNavigate } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs"
-import { useFirebase } from "./FirebaseContext";
 import { initializeApp } from "firebase/app";
 import { Loader2 } from "lucide-react"
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo } from "firebase/auth";
@@ -94,9 +92,6 @@ const Auth = () => {
               </CardHeader>
               <CardContent className="space-y-2">
                 <div className="grid grid-cols-2 gap-6">
-                  <Button className="font-bold">
-                    Other
-                  </Button>
                   <Button onClick={() => {
                     signInWithPopup(auth, googleProvider).then(async (userCredential) => {
                       let { isNewUser } = getAdditionalUserInfo(userCredential);
@@ -119,7 +114,7 @@ const Auth = () => {
                         description: "An error has occurred while signing in. Please try again later",
                       });
                     })
-                  }} className="font-bold">
+                  }} className="col-span-2 font-bold">
                     <svg role="img" viewBox="0 0 24 24" class="mr-2 h-4 w-4">
                       <path fill="currentColor" d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"></path>
                     </svg>
